3c65276add9624d2e7ce1c5f5ef3c398494b004b
mahabub <mahabub.elahi@tripegate.com>
2021-12-23 20:30:34 +0600

    inital database calling  from controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('products.index');
+
+        $products = Product::with('variants_prices_with_variants')->paginate(2);
+        return view('products.index', compact('products'));
     }
 
     /**
